9f4516a6cff88412a7c7d6f553a93223b7774ecd
Теман Зуев <temanedge95@live.com>
2018-02-08 10:39:41 +0300

    Testing push.

--- Iscariot_Desk/Iscariot_Desk/Form1.cs
+++ Iscariot_Desk/Iscariot_Desk/Form1.cs
@@ -16,5 +16,10 @@ namespace Iscariot_Desk
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK!");
+        }
     }
 }
